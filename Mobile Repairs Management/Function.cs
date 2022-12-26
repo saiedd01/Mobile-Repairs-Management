77072b3f7cf99d5172d27482f244b1b9e37ca3de
saiedd01 <sa3edahmed20@gmail.com>
2022-12-26 23:47:24 +0200

    Connection with database

---
+++ Mobile Repairs Management/Function.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Repairs_Management
+{
+    class Function
+    {
+        private SqlConnection Con;
+        private SqlCommand Cmd;
+        private DataTable dt;
+        private SqlDataAdapter sda;
+        private String Connstr;
+        
+        public Function()
+        {
+            Connstr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\Documents\MobileRepairsDB.mdf;Integrated Security=True;Connect Timeout=30";
+            Con = new SqlConnection(Connstr);
+            Cmd = new SqlCommand();
+            Cmd.Connection = Con;
+        }
+
+        public DataTable GetData(String Query)
+        {
+            dt = new DataTable();
+            sda = new SqlDataAdapter(Query, Connstr);
+            sda.Fill(dt);
+            return dt;
+        }
+
+        public int SetData(String Query)
+        {
+            int cnt = 0;
+            if(Con.State == ConnectionState.Closed)
+            {
+                Con.Open();
+            }
+            Cmd.CommandText = Query;
+            cnt = Cmd.ExecuteNonQuery();
+            return cnt;
+        }
+    }
+}
