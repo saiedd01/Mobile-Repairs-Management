bf9d2e96774089e4fe97576c7677c47c61f90f6d
saiedd01 <sa3edahmed20@gmail.com>
2022-12-27 00:05:40 +0200

    Handle Save Button in Customer Form

--- Mobile Repairs Management/Customers.cs
+++ Mobile Repairs Management/Customers.cs
@@ -12,14 +12,49 @@ namespace Mobile_Repairs_Management
 {
     public partial class Customers : Form
     {
+        Function con;
         public Customers()
         {
             InitializeComponent();
+            con = new Function();
+            ShowCustomer();
+        }
+
+        private void ShowCustomer()
+        {
+            String Query = "Select * from CustomerTbl";
+            Customerlist.DataSource = con.GetData(Query);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            if(CustNameTb.Text == "" || CustAddTb.Text == "" || CustPhoneTb.Text == "")
+            {
+                MessageBox.Show("Missing Data");
+            }
+            else
+            {
+                try
+                {
+                    String CName = CustNameTb.Text;
+                    String CPhone = CustPhoneTb.Text;
+                    String CAddress = CustAddTb.Text;
+                    String Query = "insert into CustomerTbl values ('{0}','{1}','{2}')";
+                    Query = String.Format(Query, CName, CPhone, CAddress);
+                    con.SetData(Query);
+                    MessageBox.Show("Customer Add !!!!");
+                    ShowCustomer();
+                }
+                catch (Exception Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                }
+            }
         }
     }
 }
